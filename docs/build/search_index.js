d36d72e9e1675cb51ac5fd407f5dd6a25db17e1f
Luis van Sandbergen <luis.vansandbergen@gmail.com>
2025-04-18 11:24:10 +0200

    setup docs

---
+++ docs/build/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#EAO.jl-Documentation","page":"Home","title":"EAO.jl Documentation","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"CurrentModule = EAO","category":"page"}]
+}
---
+++ docs/make.jl
@@ -0,0 +1,8 @@
+using Documenter, EAO
+
+makedocs(sitename = "EAO.jl",
+        authors = "Luis van Sandbergen", 
+        remotes= nothing,
+        pages = [
+            "Home" => "index.md"
+        ])
